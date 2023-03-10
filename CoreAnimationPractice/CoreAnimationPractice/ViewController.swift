bd0f70426d86288611509635ae04df1ca088da80
heerucan <iwpow0307@gmail.com>
2023-03-10 16:49:15 +0900

    Add: CAAnimationGroup 연습

--- CoreAnimationPractice/CoreAnimationPractice/ViewController.swift
+++ CoreAnimationPractice/CoreAnimationPractice/ViewController.swift
@@ -12,12 +12,12 @@ final class ViewController: UIViewController {
     private var practiceView = UIView()
     private var keyframeView = UIView()
     private var starView = UIView()
+    private var groupAnimationView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         configureLayout()
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,13 +25,12 @@ final class ViewController: UIViewController {
         let animation = CABasicAnimation(keyPath: "position.x")
         animation.fromValue = 100
         animation.toValue = 200
-        animation.byValue = 150
-        animation.duration = 1
-        animation.beginTime = CACurrentMediaTime()
-        animation.timeOffset = 2
-        animation.repeatCount = .infinity
-        animation.autoreverses = true
-        practiceView.layer.add(animation, forKey: nil)
+//        animation.byValue = 150
+//        animation.duration = 1
+//        animation.beginTime = CACurrentMediaTime()
+//        animation.timeOffset = 2
+//        animation.repeatCount = .infinity
+        starView.layer.add(animation, forKey: nil)
         
         let keyframeAnimation = CAKeyframeAnimation(keyPath: "backgroundColor")
         keyframeAnimation.isAdditive = true
@@ -43,11 +42,18 @@ final class ViewController: UIViewController {
         
         // 프로그램한테 어떤 애니메이션을 줄 지 알리는 것
         let starRotation = CAKeyframeAnimation(keyPath: "transform.rotation")
-        starRotation.keyTimes = [0, 0.5, 1]
+        starRotation.keyTimes = [0, 0.8, 1]
         starRotation.values = [0, Double.pi*3, Double.pi*3]
-        starRotation.duration = 10
-        starRotation.repeatCount = Float.infinity
-        practiceView.layer.add(starRotation, forKey: "transform.rotation")
+//        starRotation.duration = 10
+//        starRotation.repeatCount = Float.infinity
+        keyframeView.layer.add(starRotation, forKey: "transform.rotation")
+        
+        let rotateAndMove = CAAnimationGroup()
+        rotateAndMove.animations = [animation, starRotation]
+        rotateAndMove.duration = 2
+        rotateAndMove.repeatCount = .infinity
+        rotateAndMove.autoreverses = true
+        groupAnimationView.layer.add(rotateAndMove, forKey: "rotateAndMove")
     }
     
     private func setupUI() {
@@ -57,11 +63,14 @@ final class ViewController: UIViewController {
         keyframeView.backgroundColor = .blue
         starView = UIView(frame: CGRect(x: 100, y: 500, width: 50, height: 50))
         starView.backgroundColor = .orange
+        groupAnimationView = UIView(frame: CGRect(x: 300, y: 200, width: 40, height: 40))
+        groupAnimationView.backgroundColor = .cyan
     }
     
     private func configureLayout() {
         view.addSubview(practiceView)
         view.addSubview(keyframeView)
         view.addSubview(starView)
+        view.addSubview(groupAnimationView)
     }
 }
